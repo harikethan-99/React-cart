f05168581aabb170a9217dd70bbd94021c18b6f3
harikethan-99 <harikethan1999@gmail.com>
2021-02-06 16:32:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/css/bootstrap-grid.css';
@@ -6,16 +7,21 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {books,total} from './books';
 import Book from './book';
  function App() {  
+   
   return (
     <div className="container col-md-8 col-sm-12 bg-primary">
-      {/* {console.log(books.total)}
+      {console.log(books.total)}
       {books.map((book)=>{
         return(
-        // <Book key={book.id} {...book}></Book>the spread operator is used here we can also use book={book} wherein here
-          //remove the commented statements to pass as props//we hae passes object but in the above method we spread the object and pass it  
+        <Book 
+        key={book.id} {...book}
+        ></Book>
+        
+        //the spread operator is used here we can also use book={book} wherein here
+        //remove the commented statements to pass as props//we hae passes object but in the above method we spread the object and pass it  
         );
-      })} */}
-        <Book></Book>
+      })}
+         
       </div>
   );
 }
